9238a314b16aef2b2eb67f40905f69a2a9de3759
safronman <safronmanbox@gmail.com>
2019-02-07 08:57:12 +0300

    Updates

---
+++ precache-manifest.9b3d1e84905c5d1b26cf8f2ecf9cc69f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15c7270d331922178c67",
+    "url": "/projects/todolist/static/js/runtime~main.15c7270d.js"
+  },
+  {
+    "revision": "61bef296bce135e93302",
+    "url": "/projects/todolist/static/js/main.61bef296.chunk.js"
+  },
+  {
+    "revision": "76bd5aef46558ad2f612",
+    "url": "/projects/todolist/static/js/1.76bd5aef.chunk.js"
+  },
+  {
+    "revision": "61bef296bce135e93302",
+    "url": "/projects/todolist/static/css/main.f84f1af2.chunk.css"
+  },
+  {
+    "revision": "37ae6cfd99db1893434d8b64156f20e8",
+    "url": "/projects/todolist/index.html"
+  }
+];
